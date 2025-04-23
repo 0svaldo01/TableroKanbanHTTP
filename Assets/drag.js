251359d6b32cef08f87f77bc6cfe6df08679116c
VICTOR OSVALDO RODRIGUEZ HIDALGO <201G0283@rcarbonifera.tecnm.mx>
2025-04-23 00:41:28 -0600

    cambios

---
+++ Assets/drag.js
@@ -0,0 +1,61 @@
+ const draggables = document.querySelectorAll(".task");
+ const droppables = document.querySelectorAll(".swim-lane");
+
+draggables.forEach((task) => 
+{
+    task.addEventListener("dragstart", () => 
+        {
+            task.classList.add("is-dragging");
+        });
+    task.addEventListener("dragend" , ()=>
+        {
+            task.classList.remove("is-dragging");
+        })
+});
+
+droppables.forEach((zone) => 
+    {
+        zone.addEventListener("dragover", (e) =>
+        {
+            e.preventDefault();
+
+            const  bottomtask = insertAboveTask(zone, e.clientY);
+            const curTask= document.querySelector(".is-dragging");
+
+
+            if (!buttomtask)
+            {
+                zone.appendChild(curTask);                
+            }
+            else{
+                zone.insertBefore(curTask, bottomtask);
+            }
+
+        });
+    }
+);
+
+const insertAboveTask = (zone, mouseY) =>
+{
+    const els =zone.querySelectorAll(".task:not(.is-dragging)");
+
+    let closestTask =null;
+    let closestOffset =Number.NEGATIVE_INFINITY;
+
+    els.forEach((task)=>
+        {
+            const { top } = task.getBoundingClientRect();
+
+            const offsett = mouseY - top;
+
+            if (offsett< 0 && offsett > closestOffset)
+            {
+                closestOffset = offsett;
+                closestTask = task;
+                
+            }
+        });
+
+        return closestTask;
+
+};
